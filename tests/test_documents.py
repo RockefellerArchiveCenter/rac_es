50fadc92c4057ac16f0a9a5caeedd64acc05b5d3
Hillel Arnold <helrond@hotmail.com>
2020-06-30 22:59:11 -0400

    update tests

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -2,19 +2,20 @@ import json
 import os
 import unittest
 
+import shortuuid
 from elasticsearch.exceptions import NotFoundError
 from elasticsearch_dsl import connections
 from rac_es.documents import (Agent, BaseDescriptionComponent, Collection,
-                              DescriptionComponent, Object, Term)
+                              Object, Term)
 
 FIXTURES_DIR = "fixtures"
 
-TYPE_MAP = {
-    "agent": Agent,
-    "collection": Collection,
-    "object": Object,
-    "term": Term
-}
+TYPE_MAP = [
+    (Agent, "agent"),
+    (Collection, "collection"),
+    (Object, "object"),
+    (Term, "term")
+]
 
 
 class TestDocuments(unittest.TestCase):
@@ -29,6 +30,10 @@ class TestDocuments(unittest.TestCase):
         except NotFoundError:
             pass
         BaseDescriptionComponent.init()
+
+    def identifier_from_uri(self, uri):
+        shortuuid.set_alphabet('23456789abcdefghijkmnopqrstuvwxyz')
+        return shortuuid.uuid(name=uri)
 
     def check_source_identifiers(self, doc):
         for e in doc.external_identifiers:
@@ -46,81 +51,54 @@ class TestDocuments(unittest.TestCase):
         except AttributeError:
             pass
 
-    def expected_references(self, data, list):
-        expected = 0
-        for k in list:
-            expected += len(data.get(k, []))
-        return expected
-
-    def check_references(self, obj):
-        doc_cls = TYPE_MAP[obj.type]
-        with open(os.path.join(FIXTURES_DIR, "{}s".format(obj.type), "{}.json".format(obj.meta.id))) as jf:
-            data = json.load(jf)
-            expected = self.expected_references(
-                data,
-                set(list(getattr(doc_cls, "relations_in_self", [])) + list(getattr(doc_cls, "relations_to_self", []))))
-            self.assertEqual(len(list(obj.get_references())),
-                             expected, (obj.type, obj.meta.id))
-
     def test_document_methods(self):
         total_count = 0
-        for doc_cls, dir in [
-                (Agent, "agents"), (Collection, "collections"),
-                (Object, "objects"), (Term, "terms")]:
+        for doc_cls, dir in TYPE_MAP:
             doc_count = 0
             for f in os.listdir(os.path.join(FIXTURES_DIR, dir)):
                 with open(os.path.join(FIXTURES_DIR, dir, f), "r") as jf:
                     data = json.load(jf)
                     doc = doc_cls(**data)
-                    doc.meta.id = data["id"]
-                    doc.save(self.connection)
+                    doc.meta.id = self.identifier_from_uri(data["uri"])
+                    doc.save()
                     doc_count += 1
-                self.assertEqual(
-                    doc.component_reference, "component",
-                    "Document component_reference attribute should be `component`")
                 self.check_source_identifiers(doc)
             total_count += doc_count
             self.assertEqual(
                 doc_cls.search().count(), doc_count,
                 "Wrong number of Documents of type {} created".format(doc_cls))
 
-        for f in os.listdir(os.path.join(FIXTURES_DIR, "collections")):
-            with open(os.path.join(FIXTURES_DIR, "collections", f), "r") as jf:
+        for f in os.listdir(os.path.join(FIXTURES_DIR, "collection")):
+            with open(os.path.join(FIXTURES_DIR, "collection", f), "r") as jf:
                 data = json.load(jf)
                 doc = doc_cls(**data)
-                doc.meta.id = data["id"]
-                doc.save(self.connection)
+                doc.meta.id = self.identifier_from_uri(data["uri"])
+                doc.save()
 
         self.assertEqual(
-            DescriptionComponent.search().count(), total_count,
+            BaseDescriptionComponent.search().count(), total_count,
             "Wrong total number of documents created.")
-        for obj in DescriptionComponent.search().scan():
-            self.check_references(obj)
 
     def prepare_streaming(self, doc_cls, dir):
         for f in os.listdir(os.path.join(FIXTURES_DIR, dir)):
             with open(os.path.join(FIXTURES_DIR, dir, f), "r") as jf:
                 data = json.load(jf)
                 doc = doc_cls(**data)
+                ident = self.identifier_from_uri(data["uri"])
                 streaming_dict = doc.prepare_streaming_dict(
-                    doc["id"], self.connection)
+                    ident, self.connection)
                 self.assertTrue(isinstance(streaming_dict, dict))
-                self.assertEqual(streaming_dict["_id"], doc["id"])
                 self.assertEqual(
-                    streaming_dict["_source"]["component_reference"],
-                    "component")
+                    streaming_dict["_id"], ident)
                 yield streaming_dict
 
     def test_bulk_methods(self):
         total_count = 0
-        for doc_cls, dir, obj_type in [
-                (Agent, "agents", "agent"),
-                (Collection, "collections", "collection"),
-                (Object, "objects", "object"), (Term, "terms", "term")]:
+        for doc_cls, dir in TYPE_MAP:
             indexed = doc_cls.bulk_save(
                 self.connection,
                 self.prepare_streaming(doc_cls, dir),
-                obj_type,
+                dir,
                 1000)
             self.assertEqual(
                 doc_cls.search().count(), len(indexed),
@@ -129,8 +107,5 @@ class TestDocuments(unittest.TestCase):
             total_count += len(indexed)
 
         self.assertEqual(
-            DescriptionComponent.search().count(), total_count,
+            BaseDescriptionComponent.search().count(), total_count,
             "Wrong total number of documents created.")
-
-        for obj in DescriptionComponent.search().scan():
-            self.check_references(obj)
