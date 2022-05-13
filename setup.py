b3ac6a792fb8ababf45b4892d064aabd96ff8926
Hillel Arnold <helrond@hotmail.com>
2022-05-13 08:57:08 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='rac_es',
-    version='0.17.3',
+    version='1.0.0',
     description="Helpers for Rockefeller Archive Center's Elasticsearch implementation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
